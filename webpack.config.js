1dfd0eddca2afd757271df86a24274329fde70d5
alexKashkan <alexei.kashkan@gmail.com>
2018-11-05 13:41:48 +0300

    added url loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const webpack = require('webpack');
 
-// const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
@@ -25,17 +25,21 @@ module.exports = {
 
                 use: ['babel-loader'],
             },
+            {
+                test: /\.(png|jpg|woff|woff2|eot|ttf|svg|gif)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                use: [{ loader: 'url-loader' }],
+            },
         ],
     },
 
     plugins: [
         new CleanWebpackPlugin(['dist']),
-        // new CopyWebpackPlugin([
-        //     {
-        //         from: path.join('src', 'img'),
-        //         to: 'img',
-        //     },
-        // ]),
+        new CopyWebpackPlugin([
+            {
+                from: path.join('src', 'img'),
+                to: 'img',
+            },
+        ]),
 
         new HtmlWebpackPlugin({
             template: 'src/index.html',
